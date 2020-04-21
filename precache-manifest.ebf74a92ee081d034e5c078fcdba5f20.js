814d34944f144722f29113716bcae8c6aa7aa815
asmaa kamal <asma2_kamal2009@yahoo.com>
2020-04-21 04:39:18 +0200

    Updates

---
+++ precache-manifest.ebf74a92ee081d034e5c078fcdba5f20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c41e10e239caded72c5e300f307dd5d",
+    "url": "/reactjs-todo-list.git/index.html"
+  },
+  {
+    "revision": "c9df673f765dab1947d2",
+    "url": "/reactjs-todo-list.git/static/css/main.b6e7c412.chunk.css"
+  },
+  {
+    "revision": "4432bc0b9c448c89b35b",
+    "url": "/reactjs-todo-list.git/static/js/2.12957d55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-todo-list.git/static/js/2.12957d55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9df673f765dab1947d2",
+    "url": "/reactjs-todo-list.git/static/js/main.0ffc1153.chunk.js"
+  },
+  {
+    "revision": "f3ebac89c732df38d1a7",
+    "url": "/reactjs-todo-list.git/static/js/runtime-main.04fb179b.js"
+  }
+]);
